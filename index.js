2c0e9fc541aab3e817df628225efbef91cd76d75
Harriet Kerubo <keruboharriet7@gmail.com>
2024-01-05 04:38:09 -0500

    changing color for like & retweet icons

--- index.js
+++ index.js
@@ -54,6 +54,16 @@ function getFeedHtml() {
   let feedHtml = ``;
 
   tweetsData.forEach(function (tweet) {
+    let likeIconClass = " ";
+
+    if (tweet.isLiked) {
+      likeIconClass = "liked";
+    }
+
+    let retweetIconClass = " ";
+    if (tweet.isRetweeted) {
+      retweetIconClass = "retweeted";
+    }
     feedHtml += `
 <div class="tweet">
     <div class="tweet-inner">
@@ -69,13 +79,13 @@ function getFeedHtml() {
                     ${tweet.replies.length}
                 </span>
                 <span class="tweet-detail">
-                    <i class="fa-solid fa-heart"
+                    <i class="fa-solid fa-heart ${likeIconClass}"
                     data-like="${tweet.uuid}"
                     ></i>
                     ${tweet.likes}
                 </span>
                 <span class="tweet-detail">
-                    <i class="fa-solid fa-retweet"
+                    <i class="fa-solid fa-retweet ${retweetIconClass}"
                     data-retweet="${tweet.uuid}"
                     ></i>
                     ${tweet.retweets}
