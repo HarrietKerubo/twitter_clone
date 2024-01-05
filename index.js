8abfd0f51ae111b817e7260ac990a3ec5628d495
Harriet Kerubo <keruboharriet7@gmail.com>
2024-01-05 03:50:18 -0500

    Tweets rendered and like icon fully functional

---
+++ index.js
@@ -0,0 +1,78 @@
+import { tweetsData } from "./data.js";
+const tweetInput = document.getElementById("tweet-input");
+const tweetBtn = document.getElementById("tweet-btn");
+
+tweetBtn.addEventListener("click", function () {
+  console.log(tweetInput.value);
+});
+
+document.addEventListener("click", function (e) {
+  if (e.target.dataset.like) {
+    handleLikeClick(e.target.dataset.like);
+  }
+});
+
+// function to increament/decreament likes when like button is clicked
+function handleLikeClick(tweetId) {
+  // get the tweet object with tweetId
+  const targetTweetObj = tweetsData.filter(function (tweet) {
+    return tweet.uuid === tweetId;
+  })[0];
+
+  if (targetTweetObj.isLiked) {
+    targetTweetObj.likes--;
+  } else {
+    targetTweetObj.likes++;
+  }
+
+  //   flip isLiked to true or false if the like button is clicked/not
+
+  targetTweetObj.isLiked = !targetTweetObj.isLiked;
+
+  render();
+}
+
+function getFeedHtml() {
+  let feedHtml = ``;
+
+  tweetsData.forEach(function (tweet) {
+    feedHtml += `
+<div class="tweet">
+    <div class="tweet-inner">
+        <img src="${tweet.profilePic}" class="profile-pic">
+        <div>
+            <p class="handle">${tweet.handle}</p>
+            <p class="tweet-text">${tweet.tweetText}</p>
+            <div class="tweet-details">
+                <span class="tweet-detail">
+                    <i class="fa-regular fa-comment-dots"
+                    data-reply="${tweet.uuid}"
+                    ></i>
+                    ${tweet.replies.length}
+                </span>
+                <span class="tweet-detail">
+                    <i class="fa-solid fa-heart"
+                    data-like="${tweet.uuid}"
+                    ></i>
+                    ${tweet.likes}
+                </span>
+                <span class="tweet-detail">
+                    <i class="fa-solid fa-retweet"
+                    data-retweet="${tweet.uuid}"
+                    ></i>
+                    ${tweet.retweets}
+                </span>
+            </div>   
+        </div>            
+    </div>
+</div>
+`;
+  });
+  return feedHtml;
+}
+
+function render() {
+  document.getElementById("feed").innerHTML = getFeedHtml();
+}
+
+render();
